71fc5f4e52f67fa5b86eb1174262fa061902214e
Marco Miglierina <marco.miglierina@contentwise.tv>
2018-03-06 18:14:31 +0100

    Remove mvn command from entry point

--- 3-jdk-8/entrypoint.sh
+++ 3-jdk-8/entrypoint.sh
@@ -9,4 +9,4 @@ USER_ID=${LOCAL_USER_ID:-9001}
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
 export HOME=/home/user
 
-exec /usr/local/bin/gosu user mvn "$@"
+exec /usr/local/bin/gosu user "$@"
